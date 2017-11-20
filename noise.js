c61b7d4fd179b9015af0ad98f565672234d850b9
Lauren <Laurenelee88@gmail.com>
2017-11-20 15:03:19 -0800

    consolidating function

--- noise.js
+++ noise.js
@@ -1,3 +1,18 @@
 $(document).ready( function() {
-  // your code here
+
+  // pressing keys
+  $('body').on('keydown', function(event) {
+    let possKeys = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    if (possKeys.includes(event.key)) {
+      let audio = '#' + event.key + 'Audio'
+      $(audio).get(0).play();
+    };
+  });
+
+  // clicking boxes
+  $('.instrument').on('click', 'button', function() {
+    let audio = $('#' + $(this).html() + 'Audio');
+    $(audio).get(0).play();
+  });
+
 });
