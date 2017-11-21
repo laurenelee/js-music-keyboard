867b194cf8250f103ea40ea3a1c0150509e8cb86
Lauren <Laurenelee88@gmail.com>
2017-11-21 10:33:28 -0800

    DRY up method

--- noise.js
+++ noise.js
@@ -2,17 +2,20 @@ $(document).ready( function() {
 
   // pressing keys
   $('body').on('keydown', function(event) {
-    let possKeys = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    let possKeys = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
     if (possKeys.includes(event.key)) {
-      let audio = '#' + event.key + 'Audio'
-      $(audio).get(0).play();
-    };
+      let audio = ('#' + event.key + 'Audio');
+      audio.get(0).currentTime = 0;
+      audio.get(0).play();
+    }
   });
 
   // clicking boxes
   $('.instrument').on('click', 'button', function() {
     let audio = $('#' + $(this).html() + 'Audio');
-    $(audio).get(0).play();
+    audio.get(0).currentTime = 0;
+    audio.get(0).play();
+
   });
 
 });
